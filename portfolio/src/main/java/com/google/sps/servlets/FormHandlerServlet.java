c882c736edbebf6c980c83a4c9fd64706b1b17a7
Ryan Trinh <rqtrinh@cpp.edu>
2022-06-19 20:42:20 +0000

    Add get request to get all conact names from datastore

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,11 +1,17 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery.OrderBy;
+import com.google.gson.Gson;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.FullEntity;
 import com.google.cloud.datastore.Datastore;
@@ -17,7 +23,6 @@ public class FormHandlerServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
     // Information requested in the form
     String name = request.getParameter("name-input");
     String contactMethod = request.getParameter("contact-method");
@@ -43,5 +48,28 @@ public class FormHandlerServlet extends HttpServlet {
                                 contactMethod + ", " + contactInformation);
     // Redirect user back to main page
     response.sendRedirect("https://rtrinh-sps-summer22.appspot.com/");
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    Query<Entity> query =
+      Query.newEntityQueryBuilder().setKind("Contact").setOrderBy(OrderBy.desc("name")).build();
+    QueryResults<Entity> results = datastore.run(query);
+
+    List<String> contacts = new ArrayList<>();
+    while (results.hasNext()) {
+      Entity entity = results.next();
+
+      String contactName = entity.getString("name");
+      contacts.add(contactName);
+    }
+
+    Gson gson = new Gson();
+
+    response.setContentType("text/html;");
+    response.getWriter().println(gson.toJson(contacts));
+
   }
 }
